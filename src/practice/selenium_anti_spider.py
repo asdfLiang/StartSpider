4636f01702516c145303f8becace0c280b4a9253
asdfLiang <liangzj9624@163.com>
2024-12-23 20:27:15 +0800

    feat: CSS位置偏移反扒

---
+++ src/practice/selenium_anti_spider.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from parsel import Selector
+
+"""
+    CSS位置偏移反扒
+"""
+
+
+def parse_name(name_html: Selector):
+    if name_html.css(".whole"):
+        return name_html.css("::text").get().strip()
+
+    chars = name_html.css(".char")
+    items = []
+    for char in chars:
+        items.append(
+            {
+                "text": char.css("::text").get().strip(),
+                "left": int(char.css("::attr(style)").re_first(r"(\d+)px")),
+            }
+        )
+    items.sort(key=lambda x: x["left"])
+    return "".join([item["text"] for item in items])
+
+
+with webdriver.Chrome() as driver:
+    driver.get("https://antispider3.scrape.center/")
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".item"))
+    )
+    html = driver.page_source
+    selector = Selector(text=html)
+    names = selector.css(".item .name")
+    for name in names:
+        name = parse_name(name)
+        print(name)
